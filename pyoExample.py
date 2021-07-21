35bfecac296faa59eba769534da72b72ca8e67b9
hchristy1234 <hchristy1234@gmail.com>
2021-07-21 11:59:25 -0700

    add comments and organize files

--- pyoExample.py
+++ pyoExample.py
@@ -1,6 +1,5 @@
 from pyo import *
 from pynput import keyboard
-import os
 import time
 
 s = Server(duplex=1, buffersize=1024, winhost='asio', nchnls=2).boot()
@@ -8,7 +7,7 @@ s.start()
 
 counter = 0
 
-SNDS_PATH = "channels.wav"
+SNDS_PATH = "Sounds/channels.wav"
 white_noise = Noise(0.3)
 sound_player = SfPlayer(SNDS_PATH, loop=True,  mul=0.3)
 v = HRTF(white_noise, azimuth=0, elevation=0, mul=0.5).out()
@@ -24,22 +23,33 @@ def on_press(key):
     except AttributeError:
         print('special key {0} pressed'.format(key))
         azi = v.azimuth
+        # volume of white noise
         vol = v.mul
-        if key == keyboard.Key.left:
+        # volume of sound_player
+        cvol = c.mul
+        if key == keyboard.Key.left
+            # subtract 10 from azi to move sound to the left
             azi -= 10
-            print(azi) # check what azi value is to make sure azi is changing
+            # check what azi value is to make sure azi is changing
+            print(azi)
 
         elif key == keyboard.Key.right:
+            # subtract 10 from azi to move sound to the right
             azi += 10
-            print(azi) # check what azi value is to make sure azi is changing
+            # check what azi value is to make sure azi is changing
+            print(azi)
         elif key == keyboard.Key.up:
+            # lower the volume of v when going forward
             if round(vol, 1) != 0.0:
                 vol -= 0.1
             counter += 1
+        # after going forward 3+ steps, start playing the "c" file
         if counter > 3 and channelsToggle == False:
             c.out()
             channelsToggle == True
+        # change azi and vol values
         v.setAzimuth(float(azi))
+        c.setAzimuth(float(azi))
         v.setMul(float(vol))
 
 
