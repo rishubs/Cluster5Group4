753d7d85f51d05b4c7d30744713395960ebfddfd
hchristy1234 <hchristy1234@gmail.com>
2021-07-21 11:04:31 -0700

    add moving forward to play channels wav

--- pyoExample.py
+++ pyoExample.py
@@ -6,18 +6,25 @@ import time
 s = Server(duplex=1, buffersize=1024, winhost='asio', nchnls=2).boot()
 s.start()
 
-#SNDS_PATH = "hello.wav"
+counter = 0
+
+SNDS_PATH = "channels.wav"
 white_noise = Noise(0.3)
-#sound_player = SfPlayer(SNDS_PATH, loop=True,  mul=0.3)
+sound_player = SfPlayer(SNDS_PATH, loop=True,  mul=0.3)
 v = HRTF(white_noise, azimuth=0, elevation=0, mul=0.5).out()
+c = HRTF(sound_player, azimuth=0, elevation=0, mul=0.5)
 
 def on_press(key):
     global v
+    global c
+    global counter
+    channelsToggle = False
     try:
         print('alphanumeric key {0} pressed'.format(key.char))
     except AttributeError:
         print('special key {0} pressed'.format(key))
         azi = v.azimuth
+        vol = v.mul
         if key == keyboard.Key.left:
             azi -= 10
             print(azi) # check what azi value is to make sure azi is changing
@@ -25,7 +32,15 @@ def on_press(key):
         elif key == keyboard.Key.right:
             azi += 10
             print(azi) # check what azi value is to make sure azi is changing
+        elif key == keyboard.Key.up:
+            if round(vol, 1) != 0.0:
+                vol -= 0.1
+            counter += 1
+        if counter > 3 and channelsToggle == False:
+            c.out()
+            channelsToggle == True
         v.setAzimuth(float(azi))
+        v.setMul(float(vol))
 
 
 
