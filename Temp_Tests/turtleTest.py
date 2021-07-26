1ac7aab65096c8df260434e21cc8ad14b4fbc949
hchristy1234 <hchristy1234@gmail.com>
2021-07-26 11:15:08 -0700

    change turtle turning

--- Temp_Tests/turtleTest.py
+++ Temp_Tests/turtleTest.py
@@ -6,25 +6,28 @@ wn.title("Python Turtle Movement")
 wn.bgpic("downstairsHouse.gif")
 
 def playerUp():
-  player.sety(player.ycor()+51)
-  # player.forward(5)
+  # player.sety(player.ycor()+51)
+  player.forward(51)
 def playerDown():
-  player.sety(player.ycor()-51)
-  # player.backward(5)
+  # player.sety(player.ycor()-51)
+  player.backward(51)
 def playerRight():
-  player.setx(player.xcor()+51.5)
+  player.right(90)
+  # player.setx(player.xcor()+51.5)
   # player.right(5)
 def playerLeft():
-  player.setx(player.xcor()-51.5)
+  player.left(90)
+  # player.setx(player.xcor()-51.5)
   # player.left(5)
 def changeUp():
   wn.bgpic("upstairsGIF.gif")
 
 player = turtle.Turtle()
 player.speed(0) #this will make your player created instantly
-player.shape("square") #set player shape
+player.shape("arrow") #set player shape
 player.color("red") #set player color
 player.penup() #prevent drawing lines
+player.left(90)
 player.goto(348,-133) #set player location
 
 wn.onkey(playerUp, "w") #function, key
