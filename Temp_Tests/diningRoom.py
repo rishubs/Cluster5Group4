e77d08304d9562a07994a9c290ff484bef5a4819
rishubs <87394544+rishubs@users.noreply.github.com>
2021-07-28 09:21:44 -0800

    Delete Temp_Tests directory

--- Temp_Tests/diningRoom.py
+++
@@ -1,7 +0,0 @@
-from pyo import *
-
-s = Server(duplex=1, buffersize=1024, winhost='asio', nchnls=2).boot()
-s.start()
-
-chairsit = "../Sounds/final_chairsit.wav"
-groan = "../Sounds/final_groan.wav"
